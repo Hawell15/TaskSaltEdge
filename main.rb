0f044cd3d2e7050df3656f6c83e9a9dded036885
CiobanuSaltEdge <roma_152@mail.ru>
2019-11-27 15:15:03 +0200

    open website and login

--- main.rb
+++ main.rb
@@ -1,1 +1,23 @@
 require 'watir'
+class Bank
+  attr_accessor :browser
+
+  def openAccountPage
+    @browser=Watir::Browser.new
+    @browser.goto("https://wb.micb.md/way4u-wb/");
+    login=""
+    password=""
+    username=@browser.text_field(:class=>"username")
+    username.exists?
+    username.set login
+    username.value
+    pass=@browser.text_field(id:"password")
+    pass.exists?
+    pass.set password
+    pass.value
+    @browser.button(:class=>"wb-button").click
+  end
+end
+
+bank=Bank.new
+bank.openAccountPage
