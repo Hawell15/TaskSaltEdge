7577666a6f5df45066ba7b4e675c7f5cf3659266
CiobanuSaltEdge <roma_152@mail.ru>
2019-11-28 22:12:42 +0200

    transaction corrention

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'watir'
 require_relative 'accounts'
 require_relative 'transactions'
+
 class Bank
   attr_accessor :browser, :accounts_array
 
@@ -21,31 +22,27 @@ class Bank
   end
 
 
-public def accountInfoExtract
-@browser.div(:class=>"block__main-menu").wait_until(&:present?)
-@browser.span(:text=>"Carduri și conturi").click
-@browser.div(:class=>"contracts-section").wait_until(&:present?)
+  public def accountInfoExtract
+    @browser.div(:class=>"block__main-menu").wait_until(&:present?)
+    @browser.span(:text=>"Carduri și conturi").click
+    @browser.div(:class=>"contracts-section").wait_until(&:present?)
+    @accounts_array=Array.new
+    @transaction_array= Array.new
+    @browser.divs(:class=>"main-info").map do |acc|
+      name=acc.link(:class=>"name").title
+      currency=acc.div(:class=>["icon", "icon-account "]).text
+      amount = acc.span(:class=>"amount").text
+      acc.link(:class=>"name").click
+      @browser.link(:href=>"#contract-history").wait_until(&:present?).click
+      sleep 3
 
-@accounts_array=Array.new
-@transaction_array= Array.new
-@browser.divs(:class=>"main-info").map do |acc|
+      transactionsInfoByAccountExtract(name)
 
-    name=acc.link(:class=>"name").title
-    currency=acc.div(:class=>["icon", "icon-account "]).text
-    amount = acc.span(:class=>"amount").text
-
-  acc.link(:class=>"name").click
-
-    @browser.link(:href=>"#contract-history").wait_until(&:present?).click
-    sleep 3
-  transactionsInfoByAccountExtract(name)
-
-  @accounts_array.push(Accounts.new(name,currency,amount))
-  @browser.span(:text=>"Carduri și conturi").click
-
-  @browser.div(:class=>"contracts-section").wait_until(&:present?)
+      @accounts_array.push(Accounts.new(name,currency,amount))
+      @browser.span(:text=>"Carduri și conturi").click
+      @browser.div(:class=>"contracts-section").wait_until(&:present?)
     end
-end
+  end
 
   def transactionsInfoByAccountExtract(acc)
       if @browser.link(:class=>"operation-details").exists?
@@ -56,11 +53,9 @@ end
       end
   end
   def transactionsInfoExtract(trans)
-
       trans.link(:class=>"operation-details").wait_until(&:present?).click
       @browser.div(:class=>"details-section").wait_until(&:present?)
       i=0
-
       @browser.divs(:class=>"details-section").map do |details|
         case i
         when 0
@@ -70,16 +65,14 @@ end
         when 2
           @description=details.div(:class=>"value").text
         end
-
         i+=1
-        end
+      end
       amount=@browser.div(:class=>["details-section","amounts"]).div(:class=>"value").span(:class=>"amount").text
       currency=@browser.div(:class=>["details-section","amounts"]).div(:class=>"value").span(:class=>["amount","currency"]).text
       @browser.send_keys :escape
-
-
     return @data,@description,amount,currency,@account_name
   end
+
   def printAccount
     @accounts_array.each do |account|
       account.print
