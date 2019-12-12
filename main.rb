fb7f3798e9698a93eeb1b75b03f7187d844db301
CiobanuSaltEdge <roma_152@mail.ru>
2019-12-12 13:13:39 +0200

    changed methods

--- main.rb
+++ main.rb
@@ -4,49 +4,74 @@ require_relative 'accounts'
 require_relative 'transactions'
 
 class Bank
-  attr_accessor :browser, :accounts_array
 
-  def openAccountPage
+  def execute
+    connect
+    fetch_accounts
+    fetch_transactions
+    printAccount
+  end
+
+  def connect
+
     @browser = Watir::Browser.new
-    @browser.goto("https://wb.micb.md/way4u-wb/");
-    login = ""
-    password = ""
-    username = @browser.text_field(:class=>"username")
+    url = "https://wb.micb.md/way4u-wb/"
+    @browser.goto(url);
+
+    login = gets.chomp.to_s
+    password = gets.chomp.to_s
+
+    username = @browser.text_field(class: "username")
     username.exists?
     username.set login
     username.value
-    pass = @browser.text_field(id:"password")
+
+    pass = @browser.text_field(id: "password")
     pass.exists?
     pass.set password
     pass.value
+
     @browser.button(:class=>"wb-button").click
+
   end
 
+  def fetch_accounts
 
-  public def accountInfoExtract
     @browser.div(:class=>"block__main-menu").wait_until(&:present?)
     @browser.span(:text=>"Carduri și conturi").click
     @browser.div(:class=>"contracts-section").wait_until(&:present?)
-    @accounts_array=Array.new
 
-    doc = Nokogiri::HTML.parse(@browser.html)
-    doc.css("div.main-info").each do |acc|
+    @accounts = parse_accounts(Nokogiri::HTML(@browser.html))
+
+  end
+
+  def parse_accounts(html)
+
+    accounts_array=[]
+    html.css("div.main-info").each do |acc|
       name = acc.css("a.name").text
       currency = acc.css("div.icon.icon-account").text
-      amount = acc.css("span.amount")[0].text
+      amount = acc.at_css("span.amount").text.to_f
       account = Accounts.new(name,currency,amount)
-      @accounts_array.push(account)
+      accounts_array << account
     end
+    return accounts_array
+  end
+
+  def fetch_transactions
     @browser.divs(:class=>"main-info").map do |acc|
       name = acc.link(:class=>"name").title
       acc.link(:class=>"name").click
       @browser.link(:href=>"#contract-history").wait_until(&:present?).click
+
       sleep 3
+
       setPeriod
-      @transaction_array= Array.new
-      account = @accounts_array.detect{|a| a.name.casecmp?(name)}
+
+      account = @accounts.detect{|detected_account| detected_account.name.casecmp?(name)}
+
       transactionsInfoByAccountExtract(name,account)
-      account.transactions=@transaction_array
+
       @browser.span(:text=>"Carduri și conturi").click
       @browser.div(:class=>"contracts-section").wait_until(&:present?)
     end
@@ -55,9 +80,11 @@ class Bank
   def setPeriod
     @browser.link(:href=>"#contract-history").wait_until(&:present?).click
     @browser.div(:class=>"filters").wait_until(&:present?)
+
     time = Date.today << 2
     @browser.input(:name=>"from").click
     i = 1
+
     @browser.div(:class=>"ui-datepicker-title ").browser.divs(:class=>"arrow").map do |arrow|
       if i == 3 then
         break
@@ -75,28 +102,28 @@ class Bank
 
   def transactionsInfoByAccountExtract(acc,account)
     if @browser.link(:class=>"operation-details").exists?
-      @browser.spans(:class=>"history-item-description").map do |trans|
-        data, description, amount, currency=transactionsInfoExtract(trans)
-        @transaction_array.push(Transactions.new(data, description, amount, currency, acc))
+    @browser.spans(:class=>"history-item-description").map do |trans|
+      trans.link(:class=>"operation-details").wait_until(&:present?).click
+      @browser.div(:class=>"details-section").wait_until(&:present?)
+      parse_transactions(account,Nokogiri::HTML(@browser.html))
       end
     end
   end
 
-  def transactionsInfoExtract(trans)
-     trans.link(:class=>"operation-details").wait_until(&:present?).click
-     @browser.div(:class=>"details-section").wait_until(&:present?)
-     details = Nokogiri::HTML.parse(@browser.html)
-     data = details.css("div.details-section")[0].css("div.value").text
-     account_name = details.css("div.details-section")[1].css("div.value").text
-     description = details.css("div.details-section")[2].css("div.value").text
-     amount = details.css("div.details-section.amounts").css("span.amount")[0].text
-     currency = details.css("div.details-section.amounts").css("span.amount.currency").text
+ def parse_transactions(account, html)
+     data = html.at_css("div.details-section").at_css("div.value").text
+     account_name = html.css("div.details-section")[1].css("div.value").text
+     description = html.css("div.details-section")[2].css("div.value").text
+     amount = html.css("div.details-section.amounts").at_css("span.amount").text
+     currency = html.css("div.details-section.amounts").css("span.amount.currency").text
+
+     account.transactions<< Transactions.new(data, description, amount, currency, account.name)
      @browser.send_keys :escape
-     return data, description, amount, currency, account_name
+
   end
 
   def printAccount
-    @accounts_array.each do |account|
+    @accounts.each do |account|
       account.print
     end
   end
@@ -104,6 +131,4 @@ class Bank
 end
 
 bank = Bank.new
-bank.openAccountPage
-bank.accountInfoExtract
-bank.printAccount
+bank.execute
